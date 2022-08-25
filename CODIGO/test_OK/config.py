813d00ecff2a894c32bae866d4ebf9d9f08f7cb1
Fernando Ballesteros santamaria <fballest@student.42madrid.com>
2022-08-25 13:58:42 +0200

    begining test

---
+++ CODIGO/test_OK/config.py
@@ -0,0 +1,14 @@
+SERVER_ADDR = "127.0.0.1"
+SERVER_PORT = 80
+SERVER_PORT2 = 8001
+SERVER_PORT3 = 8010
+SERVER_PORT4 = 8002
+SERVER_NAME = "localhost"
+SERVER_NAME2 = "localhost1"
+SERVER_NAME3 = "myweb"
+SERVER_NAME4 = "localhost2"
+
+AUTH_KEY = "Basic QWRtaW46YmFzaWNwYXNz"
+
+# if server receive uri longer than max uri length, it should reponse with 414
+MAX_URI_LENGTH = 100000000
